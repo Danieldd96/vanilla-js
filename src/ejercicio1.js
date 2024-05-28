72a224e4e2d3a0634488bcca8c592a53a7452b84
Danieldd96 <danieldelgado.dd270u@gmail.com>
2024-05-28 11:03:54 -0600

    Ejercicio2

--- src/ejercicio1.js
+++ src/ejercicio1.js
@@ -2,7 +2,8 @@ let contador = {
   valor: 0,
   siguiente: function () {
     // Inserte el código aquí
+    return this.valor++
   },
-};
+}
 
 export { contador }
--- src/ejercicio2.js
+++ src/ejercicio2.js
@@ -1,7 +1,12 @@
 class Contador {
   // Inserte el código aquí
+  valor=0
+  constructor(valor){
+    this.valor = valor
+  }
+  siguiente(valor){
+    return this.valor++
+  }
 }
-
-// Inserte el código aquí
-
+let instancia = new Contador()
 export { instancia }
