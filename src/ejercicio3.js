6f5a55c08f22c1ca7869eaa61d3b0e20d05aac68
Danieldd96 <danieldelgado.dd270u@gmail.com>
2024-05-29 11:54:53 -0600

    ejercicio3Daniel

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,5 +1,22 @@
 // Inserte el código aquí
+function clonarArreglo(arr){
+    return [...arr]
+}
+clonarArreglo([20,10,"dasda"])
+function clonarObjeto(obj){
+    return {...obj}
+}
+clonarObjeto({
+    nombre: "Daniel"
+})
+function sumar(num) {
 
+    
+}
+function saludar(nombre="Desconocido") {
+    return `Hola ${nombre}`;
+    
+}
 
 
 // NO MODIFICAR
