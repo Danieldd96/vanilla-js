4ce35d53eb4b70ec1911d4899876b03f3d1e70b6
Danieldd96 <danieldelgado.dd270u@gmail.com>
2024-05-30 14:35:22 -0600

    ejercicio5Daniel

---
+++ src/calculadora.js
@@ -0,0 +1,19 @@
+let calculadora = {
+    sumar:function(primerNumero,segundoNumero){
+
+        return primerNumero+segundoNumero      
+    },
+    Resta:function(primerNumero,segundoNumero){
+        return primerNumero-segundoNumero        
+    },
+    Multiplicar:function(primerNumero,segundoNumero){
+        return primerNumero*segundoNumero        
+    },
+    Dividir:function(primerNumero,segundoNumero){
+        return primerNumero/segundoNumero        
+    }
+    
+}
+
+
+export {calculadora}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
-// Inserte el código aquí
+import { calculadora } from "./calculadora";
 
+function name(operacion,num1,num2) {
+
+    if (operacion=="+") {
+      return  calculadora.sumar(num1,num2)
+    }
+    if (operacion=="-") {
+        return calculadora.Resta(num1,num2)
+    }
+    if (operacion=="*") {
+        return calculadora.Multiplicar(num1,num2)
+    }
+    if (operacion=="/") {
+        return  calculadora.Dividir(num1,num2)
+    }
+}
+
+
+
+console.log(name("/",6,2))
